ba4fe18b5f8d38ce6ed193baf79d2353d2ef0449
Ofek Haim <ofekhaim333@gmail.com>
2018-10-23 18:58:39 +0300

    Place

---
+++ Place.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    abstract class Place
+    {
+        protected double rentPrice;
+        protected string size;
+        protected bool open;
+
+        public Place(double rentPrice, string size, bool open = false)
+        {
+            this.rentPrice = rentPrice;
+            this.size = size;
+        }
+
+        public double GASRentPrice
+        {
+            get { return this.rentPrice; }
+            set
+            {
+                if (value < 0) return;
+                this.rentPrice = value;
+            }
+        }
+        public string GASsIZE
+        {
+            get { return this.size; }
+            set { this.size = value; }
+        }
+
+        public abstract void OpenThePlase();
+        public abstract void CloseThePlase();
+    }
+
+}
